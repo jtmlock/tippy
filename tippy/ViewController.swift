98a8d789f1974bc2fb2662797cdf24853e719436
ADAM SMITH <1008391695@mwce111-01.local>
2020-01-30 17:28:57 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by ADAM SMITH on 1/30/20.
+//  Copyright © 2020 ADAM SMITH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
